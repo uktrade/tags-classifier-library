29af435318ec95c1c3517235082fbf2c5f4ef77c
elcct <pawel@mikesz.com>
2021-01-18 09:31:31 +0000

    Library setup.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# tags-classifier-library
+# Tags Classifier Library
+
+A library for classifying text that helps training prediction models and run prediction on an arbitrary text.
+
+## Local development
+
+Create a new local environment using for example `pyenv`.
+Install development dependencies using `pip install -r requirements-dev.txt`.
+Make changes to the library.
+Run `pip install .` - this will install library in your current environment.
+
+### Running tests
+
+To run tests use `make pytest`.
+
+### Coding style
+
+The library is using `black` to maintain consistent coding style, `flake8` for linting and `isort` to sort imports.
+You can check your code by running `make checks`.
+
+### Update dependencies
+
+Add library dependencies to `requirements.in` and development dependencies to `requirements-dev.in`.
+
+Update dependencies using `make compile_all_requirements` and then install from updated `requirements*.txt`.
---
+++ setup.py
@@ -0,0 +1,41 @@
+"""
+Tags classifier library
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="tags-classifier-library",
+    version="0.1.0",
+    url="https://github.com/uktrade/tags-classifier-library",
+    license="MIT",
+    author="Department for International Trade",
+    description="A library for classifying text.",
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=[],
+    extras_require={
+        "test": [
+            "pytest==6.1.0",
+            "pytest-cov==2.10.1",
+            "flake8==3.8.3",
+            "wheel>=0.31.0,<1.0.0",
+            "setuptools>=38.6.0,<39.0.0",
+            "codecov",
+            "twine",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
